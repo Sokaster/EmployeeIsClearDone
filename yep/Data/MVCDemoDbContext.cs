f9da72a73a55ef017879a85254baa49a9cb4a219
unknown <danenkov.dmitriy@list.ru>
2022-11-25 15:20:55 +0300

    done

---
+++ yep/Data/MVCDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using yep.Models.Domain;
+
+namespace yep.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees {get;set;}
+    }
+}
